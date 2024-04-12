5676c5ddf64dfd206a21d0f9357f84b66229a79e
tirthoraj Mondal <tirtho.raj.2003@gmail.com>
2024-04-12 20:35:23 +0600

    0006

--- js/phone.js
+++ js/phone.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText = '13', isShowAll) => {
+const loadPhone = async (searchText, isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json();
     const phones = data.data;
@@ -104,6 +104,6 @@ const showPhoneDetails = (phone) => {
     show_detail_modal.showModal()
 }
 
-loadPhone();
+// loadPhone();
 
 
